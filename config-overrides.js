7235b1faf6bb72f318a0cea35f41820e9736069b
frankHelens <798356548@qq.com>
2019-11-09 04:47:06 +0800

    去掉打包的map

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,16 @@
 const { override, fixBabelImports } = require('customize-cra');
 
+const rewiredMap = () => config => {
+  config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+  return config;
+};
+
+process.env.GENERATE_SOURCEMAP = "false";
+
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd-mobile',
     style: 'css',
   }),
+  rewiredMap()
 );
